ea851ee0b0f97e3a932b04ee0b0cb88e895d6ed2
Daniel Tran <danieltran770@gmail.com>
2021-01-31 01:04:40 -0500

    finished

--- game.js
+++ game.js
@@ -1,76 +1,128 @@
 // Random Computer Move
-function computerPlay()
-{
-    let computerMove = ['rock', 'paper', 'scissors']
-    let number = Math.floor(Math.random() * 3);
-    return computerMove[number]
+function computerMove() {
+    let move = ['compRock', 'compPaper', 'compScissors'];
+    let random = Math.floor(Math.random() * 3);
+    return move[random];
 }
 
-// Player Move
-function playerPlay()
-{
-    let playerMove = prompt('Make a move: ')
-    return playerMove.toLowerCase().trim()
-} 
+// Start the game
+function gameStart() {
+    start = true;
+    gameOver = false;
+    playerCounter = 0;
+    computerCounter = 0;
+    gameScore();
+    roundWinner();
+}
 
 // Simulate a round
-function playRound (playerMove, computerMove)
-{
-    if (playerMove == computerMove)
-    {
-        return 'tie'
+function playRound (e) {
+    if (gameOver || !start) {
+        return;
     }
-    else if ((playerMove == 'rock' && computerMove == 'scissors') || (playerMove == 'paper' && computerMove == 'rock') || 
-             (playerMove == 'scissors' && computerMove == 'paper'))
-    {
-        return 'player'
+    compMove = computerMove();
+    userMove = e.target.id;
+    console.log(compMove)
+    if (userMove == compMove) {
+        gameScore();
+        roundWinner('tie', compMove);
     }
-    else
-    {
-        return 'computer'
+    else if ((userMove == 'compRock' && compMove == 'compScissors') || (userMove == 'compPaper' && compMove == 'compRock') || 
+             (userMove == 'compScissors' && compMove == 'compPaper')) {
+        gameScore('player');
+        roundWinner('player', compMove);
     }
-
-}
-
-// Simulate a game with 5 rounds
-function playGame()
-{
-    let playerCounter = 0
-    let computerCounter = 0
-    gameOver = false
-
-    while (!gameOver)
-    {
-        winner = playRound(playerPlay(), computerPlay())
-        if (winner == 'player')
-        {
-            playerCounter++
-            console.log('player wins the round')
-        }
-        else if (winner == 'computer')
-        {
-            computerCounter++
-            console.log('computer wins the round')
-        }
-        else 
-        {
-            console.log('tie round')
-        }
-
-        if (playerCounter == 5 || computerCounter ==5)
-        {
-            gameOver = true
-        }
-    }
-    
-    if (playerCounter > computerCounter)
-    {
-        return 'Player Wins'
-    }
-    else 
-    {
-        return 'Computer Wins'
+    else {
+        gameScore('computer');
+        roundWinner('computer', compMove);
     }
 }
 
-console.log(playGame())
+// Update game score
+function gameScore(winner) {
+    if (winner == 'player') {
+        playerCounter += 1;
+    }
+    else if (winner == 'computer') {
+        computerCounter += 1;
+    }
+    score = document.querySelector('.score');
+    para = document.querySelector('.points');
+    para.textContent = `${playerCounter}  :  ${computerCounter}`;
+    if (playerCounter == 5 || computerCounter == 5) {
+        gameOver = true;
+    }
+}
+
+// Tells user what moves were made and who won
+function roundWinner(winner, compMove) {
+    para = document.querySelector('.winner');
+    para.textContent = ''
+    if (playerCounter == 5) {
+        image(compMove);
+        winner = document.createElement('p')
+        winner.textContent = 'You win. You have defeated the computer.';
+        para.appendChild(winner)
+        start = document.querySelector('#start')
+        start.textContent ='Play Again'
+        return;
+    }
+    else if (computerCounter == 5) {
+        image(compMove);
+        winner = document.createElement('p')
+        winner.textContent = 'You Lose. The computer has defeated you. Better luck next time.';
+        para.appendChild(winner)
+        start = document.querySelector('#start')
+        start.textContent ='Play Again'
+        return;
+    }
+    if (winner == 'tie') {
+        para.textContent += ' It is a tie Round.';
+    }
+    else if (winner == 'player') {
+        para.textContent += ' You win the round.';
+    }
+    else if (winner == 'computer') {
+        para.textContent += ' The computer wins the round.';
+    }
+    else {
+        para.textContent = '';
+    }
+    image(compMove);
+
+}
+
+function image(compMove) {
+    let  img = document.querySelector('.compImg#compRock');
+    img.classList.remove('active')
+    img = document.querySelector('.compImg#compPaper');
+    img.classList.remove('active')
+    img = document.querySelector('.compImg#compScissors');
+    img.classList.remove('active')
+
+    if (compMove == 'compRock') {
+        img = document.querySelector('.compImg#compRock');
+        img.classList.add('active')
+    }
+    else if (compMove == 'compPaper') {
+        img = document.querySelector('.compImg#compPaper');
+        img.classList.add('active')
+    }
+    else if (compMove == 'compScissors') {
+        img = document.querySelector('.compImg#compScissors');
+        img.classList.add('active')
+    }
+}
+
+let playerCounter = 0;
+let computerCounter = 0;
+let start = false;
+let gameOver = false;
+
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', gameStart);
+
+const userButtons = document.querySelectorAll('.playerMove');
+userButtons.forEach(button => {
+    button.addEventListener('click', playRound);
+});
